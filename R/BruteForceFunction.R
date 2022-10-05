509a8c71a3b04686b74f3bfab40009c9f98bf3f7
Annabasiljose <annabasiljose1@gmail.com>
2022-10-05 15:23:37 +0200

    Brute force optimization

--- R/BruteForceFunction.R
+++ R/BruteForceFunction.R
@@ -11,44 +11,22 @@ brute_force_knapsack <- function(x, W) {
 
   stopifnot(is.data.frame(x))
   stopifnot(W > 0)
-  df_colnames <- names(x)
   stopifnot(length(x) == 2)
-  stopifnot(df_colnames[1] == "w" && df_colnames[2] == "v")
+  stopifnot(names(x)[1] == "w" && names(x)[2] == "v")
 
   len <- length(x$w)
-  poss <- c(0 : (( 2 ^ len ) - 1))
-  bits <- sapply(poss, intToBits)
-  weight <- vector(length = length(poss))
-  value <- vector(length = length(poss))
+  weight <- vector(length = length(c(1 : (( 2 ^ len ) - 1))))
+  value <- vector(length = length(c(1 : (( 2 ^ len ) - 1))))
   df_new <- data.frame(weight, value)
   elements <- list()
-  for(i in c(1 : length(poss))){
-    val <- vector(length = length(poss))
-    wei <- vector(length = length(poss))
-    ele <- vector()
-    for(j in c(1 : 32)){
-      if(bits[j, i] == 1){
-        wei[i] <- wei[i] + x$w[j]
-        val[i] <- val[i] + x$v[j]
-        ele <- c(ele, j)
-      }else{
-        wei[i] <- wei[i]
-        val[i] <- val[i]
-      }
-    }
-    if(isTRUE(wei[i] <= W)){
-      df_new$weight[i] <- wei[i]
-      df_new$value[i] <- val[i]
-      elements[i] <- c(list(ele))
-    } else {
-      df_new$weight[i] <- 0
-      df_new$value[i] <- 0
-      elements[i] <-  c(0)
+  for(i in c(1 : ( 2 ^ len ) - 1)) {
+    wei <- sum(x$w[which(intToBits(i)[1:len] == 1)])
+    if(wei <= W) {
+      df_new$weight[i] <- wei
+      df_new$value[i] <- sum(x$v[which(intToBits(i)[1:len] == 1)])
+      elements[i] <- list(which(intToBits(i)[1:len] == 1))
     }
   }
-  index <- which.max(df_new$value)
-  elem <- unlist(elements[index])
-  max_va <- round(max(df_new$value))
-  result <- list(value = max_va, elements = elem)
+  result <- list(value = round(max(df_new$value)), elements = unlist(elements[which.max(df_new$value)]))
   return(result)
 }
